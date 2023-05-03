ab048d6138a4dbb10e11ae03c8a70688e85e5d8b
olandim2 <30701287+olandim2@users.noreply.github.com>
2023-05-03 14:24:12 -0300

    Mensagem

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 03 May 2023 17:24:01 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Wed, 03 May 2023 17:24:01 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
